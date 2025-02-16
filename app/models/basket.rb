7fcdec25c38cc611eec2b1dbf76fc6861a9f55e6
Stephen McCullough <stephen@skillfulgorilla.com>
2025-02-16 03:51:26 +0000

    ✨ - added shopping list based on the basket

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -3,6 +3,4 @@ class Basket < ApplicationRecord
     belongs_to :recipe
   
     validates :recipe, uniqueness: { scope: :user, message: "is already in your basket" }
-  
-    attribute :chosen, :boolean, default: false
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,30 @@ class User < ApplicationRecord
   def in_basket?(recipe)
     baskets.exists?(recipe: recipe)
   end
+
+  def aggregated_ingredients
+    db_adapter = ActiveRecord::Base.connection.adapter_name.downcase
+
+    json_query = if db_adapter.include?("sqlite")
+      "json_each(recipes.ingredients)"
+    else
+      "jsonb_array_elements(recipes.ingredients::jsonb)"
+    end
+
+    recipes.joins("JOIN #{json_query} AS ingredient")
+           .select("ingredient.value ->> 'name' AS name, 
+                    SUM(CAST(ingredient.value ->> 'quantity' AS NUMERIC)) AS total_quantity, 
+                    ingredient.value ->> 'unit' AS unit")
+           .group("name, unit")
+           .map { |record| { name: record.name, quantity: record.total_quantity, unit: record.unit } }
+  end
+
+  def ingredients_list
+    baskets.includes(:recipe).map do |basket|
+      {
+        recipe_name: basket.recipe.title,
+        ingredients: basket.recipe.ingredients
+      }
+    end
+  end
 end
