8a77f4abb89338ab2a0a815f9c86e8c545a0277a
Stephen McCullough <stephen@skillfulgorilla.com>
2025-02-16 04:08:36 +0000

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,18 +13,21 @@ class User < ApplicationRecord
   def aggregated_ingredients
     db_adapter = ActiveRecord::Base.connection.adapter_name.downcase
 
-    json_query = if db_adapter.include?("sqlite")
-      "json_each(recipes.ingredients)"
+    if db_adapter.include?("sqlite")
+      recipes.joins("JOIN json_each(recipes.ingredients) AS ingredient")
+             .select("ingredient.value ->> '$.name' AS name, 
+                      SUM(CAST(ingredient.value ->> '$.quantity' AS NUMERIC)) AS total_quantity, 
+                      ingredient.value ->> '$.unit' AS unit")
+             .group("name, unit")
+             .map { |record| { name: record.name, quantity: record.total_quantity.to_f, unit: record.unit } }
     else
-      "jsonb_array_elements(recipes.ingredients::jsonb)"
+      recipes.joins("CROSS JOIN LATERAL jsonb_array_elements(recipes.ingredients::jsonb) AS ingredient")
+             .select("ingredient->>'name' AS name, 
+                      SUM((ingredient->>'quantity')::NUMERIC) AS total_quantity, 
+                      ingredient->>'unit' AS unit")
+             .group("ingredient->>'name', ingredient->>'unit'")
+             .map { |record| { name: record.name, quantity: record.total_quantity.to_f, unit: record.unit } }
     end
-
-    recipes.joins("JOIN #{json_query} AS ingredient")
-           .select("ingredient.value ->> 'name' AS name, 
-                    SUM(CAST(ingredient.value ->> 'quantity' AS NUMERIC)) AS total_quantity, 
-                    ingredient.value ->> 'unit' AS unit")
-           .group("name, unit")
-           .map { |record| { name: record.name, quantity: record.total_quantity, unit: record.unit } }
   end
 
   def ingredients_list
