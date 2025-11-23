70e1f1fd5b0fe5423504645e441dcc4bdfa6356d
Stephen McCullough <me@swm.cc>
2025-11-23 00:59:20 +0000

    🗄️ Migrate from SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_15_234242) do
+ActiveRecord::Schema[8.1].define(version: 2025_02_15_234242) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -50,39 +53,39 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_15_234242) do
   end
 
   create_table "baskets", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "recipe_id", null: false
     t.datetime "created_at", null: false
+    t.integer "recipe_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["recipe_id"], name: "index_baskets_on_recipe_id"
     t.index ["user_id", "recipe_id"], name: "index_baskets_on_user_id_and_recipe_id", unique: true
     t.index ["user_id"], name: "index_baskets_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "serves"
-    t.text "instructions"
-    t.string "prep_time"
-    t.json "ingredients"
-    t.json "nutrition"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.json "ingredients"
+    t.text "instructions"
+    t.json "nutrition"
+    t.string "prep_time"
+    t.integer "serves"
+    t.string "title"
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_recipes_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
